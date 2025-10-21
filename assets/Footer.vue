a99c501d3bf31ff56b5df4baefe139eee3b5fe44
nanima <104359792+qazwsx230919@users.noreply.github.com>
2025-10-21 16:29:21 +0800

    Update Footer.vue

--- assets/Footer.vue
+++ assets/Footer.vue
@@ -22,8 +22,8 @@
     <div class="project-links">
       <strong>项目:</strong><a href="https://github.com/willow-god/FlareDrive-R2" target="_blank"
         rel="noopener noreferrer">FlareDrive-R2</a>
-      <strong>作者:</strong><a href="https://github.com/willow-god" target="_blank"
-        rel="noopener noreferrer">LiuShen</a>
+      <strong>作者:</strong><a href="https://github.com/ljxi/Cloudflare-R2-oss" target="_blank"
+        rel="noopener noreferrer">黑白灰</a>
       <strong>初始项目:</strong><a href="https://github.com/ljxi/Cloudflare-R2-oss" target="_blank"
         rel="noopener noreferrer">Cloudflare-R2-OSS</a>
     </div>
@@ -40,9 +40,9 @@ export default {
   name: "Footer",
   data() {
     return {
-      homeUrl: "https://www.liushen.fun/",
-      blogUrl: "https://blog.liushen.fun/",
-      githubUrl: "https://github.com/willow-god",
+      homeUrl: "https://pan1.cnmbyd.xyz/",
+      blogUrl: "https://github.com/ljxi/Cloudflare-R2-oss",
+      githubUrl: "https://github.com/ljxi/Cloudflare-R2-oss",
       emailUrl: "mailto:01@liushen.fun"
     };
   }
